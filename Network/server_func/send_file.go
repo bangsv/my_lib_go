ca5b831f3d716244967a1e2e0080709f116624a5
Zakharchuk Yuri <97832253+bangsv@users.noreply.github.com>
2023-01-25 22:15:10 +0300

    Update send_file.go

--- Network/server_func/send_file.go
+++ Network/server_func/send_file.go
@@ -9,15 +9,26 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// func ServeFile() handles requests to serve a particular file. 
+// It takes a ResponseWriter (w) and a Request (r) as arguments. 
 func ServeFile(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id := vars["id"]
-	file, err := ioutil.ReadFile(id)
-	if err != nil {
-		http.Error(w, "404 not found", http.StatusNotFound)
-		return
-	}
-	contentType := mime.TypeByExtension(filepath.Ext(id))
-	w.Header().Set("Content-Type", contentType)
-	w.Write(file)
+    // extract the "id" parameter from the URL
+    vars := mux.Vars(r)
+    id := vars["id"]
+    
+    // read the file into memory with ioutil.ReadFile 
+    file, err := ioutil.ReadFile(id)
+    
+    // if an error occurs, return a 404 not found response
+    if err != nil {
+        http.Error(w, "404 not found", http.StatusNotFound)
+        return
+    }
+    
+    // set the Content-Type of the response based on the file extension
+    contentType := mime.TypeByExtension(filepath.Ext(id))
+    w.Header().Set("Content-Type", contentType)
+    
+    // write the file to the ResponseWriter
+    w.Write(file)
 }
